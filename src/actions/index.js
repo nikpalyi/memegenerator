c80e00900e0661be7867b2513b8ac36a7fd835dc
nikpalyi <nikolett.palyi@wipro.com>
2019-08-07 16:00:15 +0100

    STORE and Thunk setup

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,26 @@
 export const RECEIVE_MEMES = 'RECEIVE_MEMES';
 
 //regular actionCreator: receiving json data -> returning action object with type and actual data
-export const receiveMemes(json){
-    const {memes} = json.data;
-    return {
-        type: RECEIVE_MEMES,
-        memes
-    }
+function receiveMemes(json) {
+  const { memes } = json.data;
+  return {
+    type: RECEIVE_MEMES,
+    memes
+  };
 }
 
 // fetch ->return a response of json
-function fetchMemesJson(){
-    return fetch('hhtps://api.imgflip.com/get_memes')
-    .then(response => response.json())
+function fetchMemesJson() {
+  return fetch('https://api.imgflip.com/get_memes').then(response =>
+    response.json()
+  );
 }
 
 // inner function , dispatch function dispatch ???
-export function fetchMemes(){
-    return function(dispatch){
-        return fetchMemesJson()
-        .then(json => disptach (receiveMemes(json)))
-    }
+export function fetchMemes() {
+  return function(dispatch) {
+    return fetchMemesJson().then(json => dispatch(receiveMemes(json)));
+  };
 }
 
 //async action creator behaviour
