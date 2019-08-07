dfa4ddd60dc821abbc85811f3a5354b83c7d1ef0
nikpalyi <nikolett.palyi@wipro.com>
2019-08-07 17:42:46 +0100

    Form setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../styles/index.css';
 
+import { Form, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
 import MemeItem from './MemeItem';
 
 class App extends Component {
@@ -17,6 +18,18 @@ class App extends Component {
     return (
       <div>
         <h2>Welcome to the Meme Generator!</h2>
+        <h4>
+          <i>Write Some Text</i>
+        </h4>
+        <Form inline>
+          <FormGroup>
+            <FormLabel>Top</FormLabel> <FormControl type='text' />
+          </FormGroup>{' '}
+          <FormGroup>
+            <FormLabel>Bottom</FormLabel>
+            <FormControl type='text' />
+          </FormGroup>
+        </Form>
         {this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
           return <MemeItem key={index} meme={meme} />;
         })}
